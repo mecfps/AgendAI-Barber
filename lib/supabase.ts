c4e81d381049bb8379496157834b585db2ffdeb5
mecfps <106492597+mecfps@users.noreply.github.com>
2025-05-18 23:01:13 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/auth-helpers-nextjs"
+import { createClient } from "@supabase/supabase-js"
 import { supabaseConfig } from "./supabase-config"
 
 // Criando cliente para o lado do servidor
